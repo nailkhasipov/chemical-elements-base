70643a52292f3cb7a92194a01346ea3f616e8d1b
Marsel Kadrov <marselkadroff@gmail.com>
2016-07-26 00:07:41 +0500

    Небольшое обновление в интерфейсе

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $( document ).ready(function(){
   $.getJSON( "database.json", function( database ) {
     window.database = database;
     $.each( database, function( index, element ){
-      var elementNode = "<li><a class='element-link' target=" + index + " href='javascript:;'>" + element["Название"] + "</a></li>";
+      var elementNode = "<li class='element-li'><a class='element-link' target=" + index + " href='javascript:;'>" + element["Название"] + "</a></li>";
       $(".sidebar").append( elementNode );
     });
   });
@@ -10,15 +10,15 @@ $( document ).ready(function(){
   $(document).on('click', '.element-link', function(e) {
     var elementId = $(this).attr("target");
     var element = window.database[elementId];
-    var table = $('<table/>');
+    var table = $('<table cellspacing="0" border="1"/>');
     var tr;
     for ( property in element ) {
       console.log(property);
       tr = $('<tr/>');
-      tr.append("<td>" + property + "</td>");
+      tr.append("<th>" + property + "</th>");
       tr.append("<td>" + element[property] + "</td>");
       $(table).append(tr);
     }
-    $(".main").html(table);
+    $(".main-table").html(table);
   });
 });
